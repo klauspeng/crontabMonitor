b9fc38c61f05444dc6703f1b0442cd1a2a6f9900
klaus <2511634167@qq.com>
2018-05-30 21:32:18 +0800

    分支测试

---
+++ tasks/Dmall2.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Project :crontabMonitor
+ * User: Klaus
+ * Date: 2018.04.03 21:24
+ */
+class Dmall2 extends \Core\TaskBase
+{
+    private $signCacheKey = 'dmall2_sign';
+
+    public function run()
+    {
+        // 签到
+        $this->signIn();
+    }
+
+    /**
+     * 签到
+     */
+    public function signIn()
+    {
+
+        // 判断今天是否签到
+        if ($this->cache->get($this->signCacheKey)) {
+            return FALSE;
+        }
+
+        // get请求
+        $this->curl->get($this->config['singUrl']);
+
+        // 校验请求结果
+        if ($this->curl->error) {
+            info('请求失败:', $this->curl->errorCode . ': ' . $this->curl->errorMessage);
+            return FALSE;
+        }
+
+        // 解析数据
+        $data = $this->curl->response;
+
+        // 校验是否成功
+        if (strpos($data, '执行签到任务成功') === FALSE) {
+            sendEmail('多点签到失败！', '多点签到失败！更换签到链接！');
+        } else {
+            $this->cache->set($this->signCacheKey, 1, getExpireTime());
+        }
+
+        info('多点签到结果：', $data);
+
+    }
+}
