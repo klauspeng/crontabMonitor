8e11a5e3cb5c4f041064be7d751a80a0bb90c4c3
liupeng21 <liupeng21@lenovo.com>
2019-02-11 17:24:06 +0800

    多点奖励领取

--- tasks/Dmall.php
+++ tasks/Dmall.php
@@ -39,9 +39,53 @@ class Dmall extends \Core\TaskBase
             sendEmail('多点签到失败！', '多点签到失败！更换签到链接！');
         } else {
             $this->cache->set($this->signCacheKey, 1, getExpireTime());
+            // 奖励领取
+            $this->getAward();
         }
 
         info('多点签到结果：', $data);
+
+    }
+
+    /**
+     * 领取奖励 -- 签到成功之后领取
+     */
+    public function getAward()
+    {
+        // GET请求
+        $this->curl->get($this->config['infoUrl']);
+        if ($this->curl->error) {
+            info('请求失败:', $this->curl->errorCode . ': ' . $this->curl->errorMessage);
+            return FALSE;
+        }
+        $data = $this->curl->response;
+        $data = str_replace(["jQuery214009259084913173321_1549874781187({'result':", '})'], '', $data);
+        $data = json_decode($data, TRUE);
+
+        if ($data['code'] == '0000') {
+            foreach ($data['data']['currentMonthContinueProgress'] as $currentMonthContinueProgress) {
+                if ($currentMonthContinueProgress['status'] == 2) {
+                    $this->curl->get($this->config['getAwardUrl'] . $currentMonthContinueProgress['taskId']);
+                    info('领取积分结果：', $this->curl->response);
+                }
+            }
+
+            foreach ($data['data']['currentMonthAddProgress'] as $currentMonthAddProgres) {
+                if ($currentMonthAddProgres['status'] == 2) {
+                    $this->curl->get($this->config['getAwardUrl'] . $currentMonthAddProgres['taskId']);
+                    info('领取积分结果：', $this->curl->response);
+                }
+            }
+
+            foreach ($data['data']['notCurrentMonthContinueProgress'] as $notCurrentMonthContinueProgres) {
+                if ($notCurrentMonthContinueProgres['status'] == 2) {
+                    $this->curl->get($this->config['getAwardUrl'] . $notCurrentMonthContinueProgres['taskId']);
+                    info('领取积分结果：', $this->curl->response);
+                }
+            }
+        } else {
+            info('领取积分失败：', $this->curl->response);
+        }
 
     }
 }
