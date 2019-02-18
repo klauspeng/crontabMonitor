7a2b792e1c34138df4b9ca8d7389ae8db9879f5d
liupeng21 <liupeng21@lenovo.com>
2019-02-18 09:37:02 +0800

    头条晒收入

--- tasks/Toutiao.php
+++ tasks/Toutiao.php
@@ -14,6 +14,7 @@ class Toutiao extends \Core\TaskBase
 {
     private $signCacheKey = 'toutiao_sign';
     private $boxCacheKey = 'toutiao_hour_box';
+    private $shareCacheKey = 'toutiao_share';
 
     public function run()
     {
@@ -22,6 +23,9 @@ class Toutiao extends \Core\TaskBase
 
         // 开启宝箱
         $this->openBox();
+
+        // 晒收入
+        $this->shareAward();
     }
 
     /**
@@ -29,7 +33,6 @@ class Toutiao extends \Core\TaskBase
      */
     public function signIn()
     {
-
         // 判断今天是否签到
         if ($this->cache->get($this->signCacheKey)) {
             return FALSE;
@@ -49,9 +52,6 @@ class Toutiao extends \Core\TaskBase
             sendEmail('今日头条签到失败！', '今日头条签到失败！更换签到链接！');
         } else {
             $this->cache->set($this->signCacheKey, 1, getExpireTime());
-
-            // 晒收入
-            $this->shareAward();
         }
 
         info('今日头条签到结果：', $data);
@@ -63,7 +63,6 @@ class Toutiao extends \Core\TaskBase
      */
     public function openBox()
     {
-
         // 判断今天是否签到
         if ($this->cache->get($this->boxCacheKey)) {
             return FALSE;
@@ -80,18 +79,22 @@ class Toutiao extends \Core\TaskBase
 
         // 校验是否成功
         if ($data['err_no'] == 0) {
-            $this->cache->set($this->boxCacheKey, 1, $data['data']['next_treasure_time'] - time());
+            $this->cache->set($this->boxCacheKey, 1, $data['data']['next_treasure_time'] - $data['data']['current_time']);
         }
 
         info('今日头条开宝箱结果：', $data);
     }
-
 
     /**
      * 晒收入
      */
     public function shareAward()
     {
+        // 判断今天是否分享
+        if ($this->cache->get($this->shareCacheKey)) {
+            return FALSE;
+        }
+
         // 设置cookie POST请求
         $this->curl->setHeader('Content-Type', 'application/json');
         $this->curl->setCookieString($this->config['cookie']);
@@ -101,6 +104,8 @@ class Toutiao extends \Core\TaskBase
             $data = stdObjectToArray($this->curl->response);
             info('今日头条晒收入结果：', $data);
         }
+
+        $this->cache->set($this->shareCacheKey, 1, getExpireTime());
     }
 
 }
