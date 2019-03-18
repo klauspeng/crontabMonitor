e0a231f84eff067f827ed6377965456098cfb5d6
KlausPeng <2511634167@qq.com>
2019-03-18 08:25:04 +0800

    东方头条 时段奖励

---
+++ tasks/Dongfang.php
@@ -0,0 +1,35 @@
+<?php
+
+use Core\TaskBase;
+
+/**
+ * Desc : Dongfang.php 东方头条
+ * User : liupeng
+ * Date : 2019/3/18
+ */
+class Dongfang extends TaskBase
+{
+    private $hourCacheKey = 'dongfang_hour_award';
+
+    public function run()
+    {
+        $this->getHourAward();
+    }
+
+    /**
+     * 获取时段奖励
+     */
+    public function getHourAward()
+    {
+        // 判断今天是否获取
+        if ($this->cache->get($this->hourCacheKey)) {
+            return FALSE;
+        }
+
+        $info = $this->curl->post($this->config['hourAward'], $this->config['hourAwardData']);
+        $info = stdObjectToArray($info);
+        info('东方头条时段奖励：', $info['data']);
+        $this->cache->set($this->hourCacheKey, 1, 3600);
+    }
+
+}
