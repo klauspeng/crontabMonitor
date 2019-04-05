993fdae952f42cf46c0762cb5920f6b205837ccf
KlausPeng <2511634167@qq.com>
2019-04-05 12:21:53 +0800

    东方头条 摇钱树

--- tasks/Dongfang.php
+++ tasks/Dongfang.php
@@ -10,10 +10,12 @@ use Core\TaskBase;
 class Dongfang extends TaskBase
 {
     private $hourCacheKey = 'dongfang_hour_award';
+    private $treeCacheKey = 'dongfang_tree_award';
 
     public function run()
     {
         $this->getHourAward();
+        $this->getTreeAward();
     }
 
     /**
@@ -30,6 +32,22 @@ class Dongfang extends TaskBase
         $info = stdObjectToArray($info);
         info('东方头条时段奖励：', $info['data']);
         $this->cache->set($this->hourCacheKey, 1, 3600);
+    }
+
+    /**
+     * 获取摇钱树奖励
+     */
+    public function getTreeAward()
+    {
+        // 判断今天是否获取
+        if ($this->cache->get($this->treeCacheKey)) {
+            return FALSE;
+        }
+
+        $info = $this->curl->post($this->config['treeAward'], $this->config['treeAwardData']);
+        $info = stdObjectToArray($info);
+        info('东方头条摇钱树奖励：', $info['data']);
+        $this->cache->set($this->treeCacheKey, 1, $info['data']['remaining_time']);
     }
 
 }
